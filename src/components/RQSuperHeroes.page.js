9979f5b83b2e404625468e91301028349c209860
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-16 15:01:43 +0530

    code formated

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -7,14 +7,17 @@ const fetchSuperHeroes = () => {
 };
 
 export function RQSuperHeroesPage() {
-  const { isLoading, data, isError, error } = useQuery(`super-heroes`, fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    `super-heroes`,
+    fetchSuperHeroes
+  );
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
 
-  if(isError){
-    return <h2>{error.message}</h2>
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
