a496e1218b22932c7c322adb384796711208e6b2
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-16 14:52:05 +0530

    data fetching method added using basic react query

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,29 +1,27 @@
-import axios from 'axios'
-import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 export function SuperHeroesPage() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, SetData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, SetData] = useState([]);
 
-    useEffect(() => {
-        axios.get(`http://localhost:4000/superheroes`).then((res)=>{
-            SetData(res.data)
-            setIsLoading(false)
-        })
-    }, [])
+  useEffect(() => {
+    axios.get(`http://localhost:4000/superheroes`).then((res) => {
+      SetData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
 
-    if(isLoading){
-        return <h2>Loading...</h2>
-    }
-    
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-<>
-<h2>
-    Super Hero Page
-</h2>
-{data.map((hero)=>{
-return <div key={hero.name}>{hero.name}</div>
-})}
-</>
-  )
+    <>
+      <h2>Super Hero Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 }
