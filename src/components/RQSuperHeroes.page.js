18bbb23609d2d439202b035bb5a88abcbea0327a
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-16 22:59:00 +0530

    custom query hook created and used

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,33 +1,17 @@
 import React from "react";
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchSuperHeroes = () => {
-  return axios.get(`http://localhost:4000/superheroes`);
-};
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export function RQSuperHeroesPage() {
+  const onSuccess = (data) => {
+    console.log(`Perform side effect after data fetching`, data);
+  };
 
-  const logSuccess = (data)=>{
-    console.log(`Perform side effect after data fetching`,data)
-  }
+  const onError = (error) => {
+    console.log(`Perform side effect after encountering error`, error);
+  };
 
-  const logError = (error)=>{
-    console.log(`Perform side effect after encountering error`,error)
-  }
-
-  const { isLoading, isFetching, data, isError, error, refetch } = useQuery(
-    `super-heroes`,
-    fetchSuperHeroes,
-    {
-      onSuccess:logSuccess,
-      onError:logError,
-      select: (data)=>{
-        const superHeroNames = data.data.map(hero=>hero.name)
-        return superHeroNames
-      }
-    }
-  );
+  const { isLoading, isFetching, data, isError, error, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
@@ -44,11 +28,9 @@ export function RQSuperHeroesPage() {
       {/* {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })} */}
-      {
-        data.map(heroNames=>{
-          return <div key={heroNames}>{heroNames}</div>
-        })
-      }
+      {data.map((heroNames) => {
+        return <div key={heroNames}>{heroNames}</div>;
+      })}
     </>
   );
 }
