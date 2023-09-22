2b1361bab3840a8878a43be28e2832ccfbf38719
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-22 23:19:00 +0530

    handling response data from post record

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -24,8 +24,15 @@ export const useAddSuperHeroData = () =>{
   const queryClient = useQueryClient()
 
   return useMutation(addSuperHero, {
-    onSuccess:()=>{
-      queryClient.invalidateQueries(`super-heroes`)
+    onSuccess:(data)=>{
+      // queryClient.invalidateQueries(`super-heroes`)
+      console.log(data)
+      queryClient.setQueriesData(`super-heroes`,(oldQueryData)=>{
+        return{
+          ...oldQueryData,
+          data:[...oldQueryData.data, data.data]
+        }
+      })
     }
   })
   
