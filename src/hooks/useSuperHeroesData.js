8d4851d64de4a709491a295e87d1197d16f0bc82
Devesh Mishra <devesh.mishra@intrcity.com>
2023-09-23 00:21:14 +0530

    Optimistic Updates

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 const fetchSuperHeroes = () => {
   return axios.get(`http://localhost:4000/superheroes`);
-}
+};
 
 const addSuperHero = (hero) => {
   return axios.post(`http://localhost:4000/superheroes`, hero);
-}
+};
 
 export const useSuperHeroesData = (onSuccess, onError) => {
   return useQuery(`super-heroes`, fetchSuperHeroes, {
@@ -18,22 +18,48 @@ export const useSuperHeroesData = (onSuccess, onError) => {
     //   return superHeroNames;
     // },
   });
-}
+};
 
-export const useAddSuperHeroData = () =>{
-  const queryClient = useQueryClient()
+export const useAddSuperHeroData = () => {
+  const queryClient = useQueryClient();
 
   return useMutation(addSuperHero, {
-    onSuccess:(data)=>{
-      // queryClient.invalidateQueries(`super-heroes`)
-      console.log(data)
-      queryClient.setQueriesData(`super-heroes`,(oldQueryData)=>{
-        return{
+    // onSuccess:(data)=>{
+    //   // queryClient.invalidateQueries(`super-heroes`)
+    //   queryClient.setQueriesData(`super-heroes`,(oldQueryData)=>{
+    //     return{
+    //       ...oldQueryData,
+    //       data:[...oldQueryData.data, data.data]
+    //     }
+    //   })
+    // }
+    /*---------------suppose data saving will not have any error------------*/
+    /*---------------Optimistic Updates------------*/
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries(`super-heroes`);
+      const previousHeroData = queryClient.getQueryData(`super-heroes`);
+      /*----------append new data-------*/
+      queryClient.setQueriesData(`super-heroes`, (oldQueryData) => {
+        return {
           ...oldQueryData,
-          data:[...oldQueryData.data, data.data]
-        }
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
+        };
       })
-    }
-  })
-  
-}
+      /*----------return previous catched data-------*/
+      return{
+        previousHeroData, 
+      }
+    },
+    onError: (_error, _hero, context) => {
+      /*----------set previous data if any error from api-------*/
+      queryClient.setQueryData(`super-heroes`, context.previousHeroData)
+    },
+    onSettled: () => {
+      /*----------sync data from the db-------*/
+      queryClient.invalidateQueries(`super-heroes`)
+    },
+  });
+};
